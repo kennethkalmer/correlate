c3aa656f1959c12366b7fd4b66abf439a024c96f
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-12-10 13:52:43 +0200

    Pass with all rubies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,16 @@ require 'correlate'
 require 'spec'
 require 'spec/autorun'
 
+require 'rubygems' unless ENV['NO_RUBYGEMS']
 require 'couchrest'
 
-COUCHHOST = "http://127.0.0.1:5984"
-TESTDB    = 'correlate-test'
-TEST_SERVER    = CouchRest.new
-TEST_SERVER.default_database = TESTDB
-DB = TEST_SERVER.database(TESTDB)
+unless defined?( COUCHHOST )
+  COUCHHOST = "http://127.0.0.1:5984"
+  TESTDB    = 'correlate-test'
+  TEST_SERVER    = CouchRest.new
+  TEST_SERVER.default_database = TESTDB
+  DB = TEST_SERVER.database(TESTDB)
+end
 
 Spec::Runner.configure do |config|
   config.before(:all) { reset_test_db! }
@@ -18,6 +21,7 @@ end
 
 def fixtures( *args )
   args.each do |file|
+    file = file.to_s
     if File.exists?( File.join( File.dirname(__FILE__), 'fixtures', "#{file}.rb" ) )
       require File.join( File.dirname(__FILE__), 'fixtures', file )
     end
